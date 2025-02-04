b5cae16377f8d13caf42f4b933da18d7b9bb8e30
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-02-04 16:25:28 -0500

    Update footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+// import { merriweather } from "@/styles/fonts"; // Import Merriweather from layout.tsx
 
 const Footer = () => {
     return(
         <footer className="w-full h-[300px] border-t border-black">
             <div className="w-full h-full max-w-[75%] flex justify-between items-center m-auto">
+                {/* <h1 className={`${merriweather.className} font-bold text-2xl`}>LINK AFFILIATES</h1> */}
                 <Image alt="logo" src="/logo.png" width={200} height={200} className=""/>
                 <div className="flex flex-col">
                     <h1>CONTACT US</h1>
