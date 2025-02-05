41e249d1856b1046fda8466c443e6feb63145f32
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-02-05 16:45:21 -0500

    added video

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -11,10 +11,20 @@ const Testimonials = () => {
                     <p>See how our services made a real impact—straight from a business we&apos;ve helped grow.</p>
                 </div>
                 <div className="flex">
-                    <div className="w-1/2 px-16">
-                         <h1 className="bg-gray-800 text-white w-[500px] h-[300px] text-center">VIDEO</h1>
+                    <div className="w-1/2">
+                        <video
+                            className="w-full h-auto"
+                            controls
+                            autoPlay
+                            loop
+                            muted
+                        >
+                            <source src="/video/testimonial.webm" type="video/webm" />
+                            Your browser does not support the video tag.
+                        </video>
+                        {/* <h1 className="bg-gray-800 text-white w-[500px] h-[300px] text-center">VIDEO</h1> */}
                     </div>
-                    <div className="w-1/2 flex flex-col justify-center items-start gap-4">
+                    <div className="w-1/2 flex flex-col justify-center items-start gap-4 px-16">
                         <p className="font-semibold italic">&quot;Jack Sellers and Link Affiliates created and managed advertising campaigns for me that produced an incredible conversion rate. The leads that were provided were top notch and we produced a fantastic ROI (32x) on the money we invested. Jack is also very communicative and professional to work with. Would 100% recommend the service, worth every penny.&quot;</p>
                         <p>- Justin Adams, Owner of Dynamic Design & Remodeling</p>
                         <Image src='/testimonial.png' alt="testimonial company" width={200} height={200} className="w-auto h-[40px]"></Image>
