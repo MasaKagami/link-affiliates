ba5feb6b7c358b6fbe59deac5d66e03246c4a558
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-02-09 13:00:07 -0500

    Update ourTeam.tsx

--- src/components/ourTeam/ourTeam.tsx
+++ src/components/ourTeam/ourTeam.tsx
@@ -9,9 +9,9 @@ const OurTeam = () => {
                 <div className="flex flex-col gap-10 items-center w-full">
                     <h1 className={`text-5xl font-bold ${merriweather.className} `}>Our Team</h1>
                     {/* <div className="w-full flex justify-between gap-8"> */}
-                    <div className="w-full grid xl:grid-cols-5 grid-cols-2 space-y-8 xl:space-y-0 space-x-2 xl:space-x-1">
+                    <div className="w-full grid xl:grid-cols-5 grid-cols-2 space-x-4 xl:space-x-4">
 
-                        <div className="flex-1 space-y-5">
+                        <div className="flex-1 space-y-5 my-4">
                             <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
                                 <Image src="/karen.webp" alt="photo of karen" height={200} width={200} className="w-full h-full object-cover">
                                 </Image>
@@ -22,10 +22,10 @@ const OurTeam = () => {
                                     <p className="text-sm font-medium">Co-Founder</p>
                                 </div>
                         
-                                <p className="text-xs">With an MBA in marketing & strategic planning and over 25 years of sales and marketing experience, Karen has helped her clients increase their market share and revenues. She has developed and managed successful B2B and B2C campaigns that drive results.</p>
+                                <p className="hidden sm:flex text-xs">With an MBA in marketing & strategic planning and over 25 years of sales and marketing experience, Karen has helped her clients increase their market share and revenues. She has developed and managed successful B2B and B2C campaigns that drive results.</p>
                             </div>
                         </div>
-                        <div className="flex-1 space-y-5">
+                        <div className="flex-1 space-y-5 my-4">
                             <div className="w-[200px] h-[200px] rounded-full overflow-hidden border">
                                 <Image src="/jack.webp" alt="photo of jack" height={200} width={200} className="w-full h-full object-cover object-[50%_-70%]">
                                 </Image>
@@ -35,13 +35,13 @@ const OurTeam = () => {
                                     <h1 className={`text-2xl font-bold ${merriweather.className} `}>Jack Sellers</h1>
                                     <p className="text-sm font-medium">Co-Founder & CEO</p>
                                 </div>
-                                <div className="flex flex-col gap-2 text-xs">
+                                <div className="sm:flex flex-col gap-2 hidden text-xs">
                                     <p>With a business degree from McGill University in Montreal, Canada, Jack has gone on to establish two social media marketing agencies where he has helped local businesses throughout North America grow and thrive.</p>
                                     <p>With experience in Finance, Wealth Management, and Client Management, Jack has a diverse background with different industries.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex-1 space-y-5">
+                        <div className="flex-1 space-y-5 my-4">
                             <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
                                 <Image src="/santi.webp" alt="photo of santi" height={200} width={200} className="w-full h-full object-cover">
                                 </Image>
@@ -51,13 +51,13 @@ const OurTeam = () => {
                                     <h1 className={`text-2xl font-bold ${merriweather.className} `}>Santi Vargas</h1>
                                     <p className="text-sm font-medium">Account Manager</p>
                                 </div>
-                                <div className="flex flex-col gap-2 text-xs">
+                                <div className="sm:flex flex-col gap-2 hidden text-xs">
                                     <p>With a business degree from Western University, Santiago has built a software company, leading client acquisition and marketing.</p>
                                     <p>Drawing from hands-on experience running campaigns for his own business, he applies proven strategies to optimize ads and funnels, helping his clients reach their sales goals.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex-1 space-y-5">
+                        <div className="flex-1 space-y-5 my-4">
                             <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
                                 <Image src="/masa.webp" alt="photo of masa" height={200} width={200} className="w-full h-full object-cover">
                                 </Image>
@@ -67,13 +67,13 @@ const OurTeam = () => {
                                     <h1 className={`text-2xl font-bold ${merriweather.className} `}>Nagamasa Kagami</h1>
                                     <p className="text-sm font-medium">CTO</p>
                                 </div>
-                                <div className="flex flex-col gap-2 text-xs">
+                                <div className="sm:flex flex-col gap-2 hidden text-xs">
                                     <p>Currently completing his Electrical Engineering degree with a Software Engineering minor at McGill University, Nagamasa has worked on various software projects, specializing in frontend development. Most recently working as a lead frontend engineer at a startup.</p>
                                     <p>Combining technical expertise with a creative approach, Nagamasa has built intuitive web applications that enhance user experience and streamline complex workflows.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex-1 space-y-5">
+                        <div className="flex-1 space-y-5 my-4">
                             <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
                                 <Image src="/tim.webp" alt="photo of masa" height={200} width={200} className="w-full h-full object-cover object-[0_25%]">
                                 </Image>
@@ -83,7 +83,7 @@ const OurTeam = () => {
                                     <h1 className={`text-2xl font-bold ${merriweather.className} `}>Tim Choi</h1>
                                     <p className="text-sm font-medium">Director of Finance</p>
                                 </div>
-                                <div className="flex flex-col gap-2 text-xs">
+                                <div className="sm:flex flex-col gap-2 hidden text-xs">
                                     <p>Tim is a finance professional with experience in deal advisory, audit, and business development. He holds a Bachelor of Commerce from Queen’s University’s Smith School of Business.</p>
                                     <p>With his experience, Tim has helped facilitate mid-market M&A deals, along with providing lending to SMEs.</p>
                                 </div>
