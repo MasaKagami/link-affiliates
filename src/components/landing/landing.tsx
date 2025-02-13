e33068c31c499dbfa910fa2d7b86804a553b358a
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-02-13 13:51:11 -0500

    done

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -7,8 +7,8 @@ const Landing = () => {
         <div id="home" className="relative w-full h-[calc(100vh-90px)] py-20">
             
             {/* Background Image with Blur */}
-            {/* <div className="absolute inset-0 bg-[url('/background2.svg')] bg-cover bg-center"></div> */}
-            <div className="absolute inset-0 bg-gradient-to-r from-[#bad3ff] via-[#c5f1fa] to-[#f6fcff] "></div>
+            <div className="absolute inset-0 bg-[url('/background3.jpg')] bg-cover bg-center "></div>
+            {/* <div className="absolute inset-0 bg-gradient-to-r from-[#bad3ff] via-[#c5f1fa] to-[#f6fcff] "></div> */}
 
             {/* Content with a backdrop effect */}
             <div className="relative w-full h-full flex max-w-[90%] sm:max-w-[75%] m-auto items-center ">
@@ -17,15 +17,28 @@ const Landing = () => {
                     </Image> */}
                 </div>
 
-                {/* <div className="flex flex-col gap-5 justify-center items-start w-[95%] lg:w-1/2 bg-white backdrop-blur-sm p-8 shadow-[0_0_30px_rgba(255,255,255,0.5)]"> */}
-                <div className="flex flex-col gap-5 justify-center items-start w-full lg:w-1/2 p-8 ">
-
+                {/* <div className="flex flex-col gap-5 justify-center items-start w-full lg:w-1/2 p-8 ">
+                <div className="flex flex-col gap-5 justify-center items-start w-[95%] lg:w-1/2 bg-opacity-45 backdrop-blur-sm p-8 shadow-[0_0_30px_rgba(255,255,255,0.5)]">
                     <h1 className={`text-4xl lg:text-5xl font-bold ${merriweather.className} `}>Exclusively for Renovation and Remodelling Businesses in North America</h1>
                     <p>Growing local businesses&apos; clientele by generating qualified leads. Our process is simple, yet effective. Find out how we fill your schedule with new clients.</p>
-                    <MeetingButton/>
-                    
+                    <MeetingButton/>                    
                 </div>
-                
+                 */}
+
+                 
+                <div className={`flex flex-col gap-5 justify-center items-start w-full lg:w-1/2 
+                    bg-white/10 backdrop-blur-lg p-8 rounded-lg 
+                    shadow-[0_0_50px_rgba(173,216,230,0.5)]`}>
+                    <h1 className={`text-4xl lg:text-5xl font-bold ${merriweather.className}`}>
+                        Exclusively for Renovation and Remodelling Businesses in North America
+                    </h1>
+                    <p>
+                        Growing local businesses&apos; clientele by generating qualified leads. Our process is simple, yet effective. 
+                        Find out how we fill your schedule with new clients.
+                    </p>   
+                    <MeetingButton/>                    
+                </div>
+
             </div>
         </div>
     )
