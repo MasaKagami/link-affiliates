deb438193ce79418c309dc87346e7e14bd11a25d
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-02-13 13:35:08 -0500

    Update meetingButton.tsx

--- src/components/meetingButton/meetingButton.tsx
+++ src/components/meetingButton/meetingButton.tsx
@@ -4,7 +4,7 @@ const MeetingButton = () => {
     return(
         <Link href="/book">
             <button className="cta flex items-center">
-                <span>Book a meeting</span>
+                <span className="text-black">Book a meeting</span>
                 <svg width="15px" height="10px" viewBox="0 0 13 10">
                     <path d="M1,5 L11,5"></path>
                     <polyline points="8 1 12 5 8 9"></polyline>
